ae080c481f1e17a740e5c1ce5fc8ff5cf5e18e6a
Alex Perez <alexjperez@gmail.com>
2015-09-03 10:48:26 -0700

    Edited add paths

--- MSI3D/compileCPD.m
+++ MSI3D/compileCPD.m
@@ -2,12 +2,7 @@ function compileCPD
 % compileCPD
 %    Adds the appropriate paths and compiles the CPD package.
 
-addpath C:\Users\Alex\Documents\NCMIR\Experiments\MSI3D\nucleolus_test\keep\CPD2\core
-addpath C:\Users\Alex\Documents\NCMIR\Experiments\MSI3D\nucleolus_test\keep\CPD2\core\utils
-addpath C:\Users\Alex\Documents\NCMIR\Experiments\MSI3D\nucleolus_test\keep\CPD2\core\Rigid
-addpath C:\Users\Alex\Documents\NCMIR\Experiments\MSI3D\nucleolus_test\keep\CPD2\core\Nonrigid
-addpath C:\Users\Alex\Documents\NCMIR\Experiments\MSI3D\nucleolus_test\keep\CPD2\core\mex
-addpath C:\Users\Alex\Documents\NCMIR\Experiments\MSI3D\nucleolus_test\keep\CPD2\core\FGT
+addpath 'CPD/core' 'CPD/core/utils' 'CPD/core/Rigid' 'CPD/core/Nonrigid' 'CPD/core/mex' 'CPD/core/FGT'
 cpd_make;
 
 end
