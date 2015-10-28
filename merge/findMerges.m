b46b1be8d14fd6d0e3e0c0ad50ffd021912be535
Alex Perez <alexjperez@gmail.com>
2015-10-28 15:53:18 -0700

    Added support for merging in the negative Z direction

--- merge/findMerges.m
+++ merge/findMerges.m
@@ -1,4 +1,4 @@
-function merge = findMerges( N, cc, objects, opts )
+function merge = findMerges( N, cc, objects, opts, direction)
 %
 %
 %
@@ -7,20 +7,32 @@ function merge = findMerges( N, cc, objects, opts )
 % Get the Z values for every pixel index in the given object
 [~, ~, z] = ind2sub(opts.stackdims, cc.PixelIdxList{N});
 
-% Get the indices that correspond to the pixels on the maximum slice
-vals = (z == cc.zmax(N));
-idx = cc.PixelIdxList{N}(vals);
-area = numel(idx);
+% Get the indices that correspond to the pixels on the maximum slice, if the
+% positive direction is desired, or the minimum slice, if the negative 
+% direction is desired.
+if direction == 1
+    vals = (z == cc.zmax(N));
+    idx = cc.PixelIdxList{N}(vals);
+    area = numel(idx);
+elseif direction == -1
+    vals = (z == cc.zmin(N));
+    idx = cc.PixelIdxList{N}(vals);
+    area = numel(idx);
+end
 
 % Loop over all values of skip, from one to the max
 
 toggle = 0;
-for ii = 1:opts.skip
-                  
+slices_ii = direction * [1:opts.skip];
+for jj = 1:numel(slices_ii)
+    
+    ii = slices_ii(jj);
+
     % Determine which pixel indices on the above slice to test for
-    idxskip = bsxfun(@plus, idx, opts.idxPerSlice * (ii+1));
+    idxskip = bsxfun(@plus, idx, opts.idxPerSlice * (ii + direction));
     idxskip = reshape(idxskip, numel(idxskip), 1); %May no longer be necessary
     idxskip = idxskip(idxskip < opts.idxMax); %Remove any invalid indices
+    idxskip = idxskip(idxskip > 0);
         
     % Determine how many pixels in idxskip overlap with the actual
     % segmentation
@@ -43,7 +55,7 @@ for ii = 1:opts.skip
         % merged into object ii. To do this, we will look in all
         % objects that have values on the given slice. These are stored
         % in the structure slices.
-        searchStr = ['z' sprintf('%04d', cc.zmax(N) + ii + 1)];
+        searchStr = ['z' sprintf('%04d', cc.zmax(N) + ii + direction)];
         searchObjects = objects.(searchStr);
             
         % Loop over each object in searchObjects. Determine if its
@@ -56,7 +68,7 @@ for ii = 1:opts.skip
                 fprintf('Merge found: %d\n', obj_kk);
                 
                 % Merge the PixelIdxList entries for the two objects
-                cc.PixelIdxList{ii} = [cc.PixelIdxList{ii}; 
+                cc.PixelIdxList{N} = [cc.PixelIdxList{N}; 
                     cc.PixelIdxList{obj_kk}];
                 cc.PixelIdxList{obj_kk} = [];
                 break
